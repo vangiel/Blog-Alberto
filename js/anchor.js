a3905f2d69e3164b23261be6f05113ab1522a510
Daiel Rodriguez <dani1610@hotmail.com>
2017-03-06 00:10:22 +0100

    Menu y página de contacto

--- js/anchor.js
+++ js/anchor.js
@@ -1,8 +1,33 @@
 /**
  * Created by Daniel on 16/02/2017.
  */
+
+//Anchor
+
 $('a.continue__button').click(function(){
   $('html, body').animate({
     scrollTop: $( $(this).attr('href') ).offset().top
   }, 800);
+  $('.main-nav').addClass("fixed");
+});
+
+// Sticky menu
+
+var scroll;
+var flag = false;
+
+$(window).scroll(function () {
+  scroll = $(window).scrollTop();
+
+  if(scroll > 400){
+    if (!flag) {
+      $('.main-nav').addClass("fixed");
+      flag = true;
+    }
+  }else{
+    if (flag) {
+      $('.main-nav').removeClass("fixed");
+      flag = false;
+    }
+  }
 });
